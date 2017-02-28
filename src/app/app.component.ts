8e7188a6369f420fd2057fb07ab5fbdb2f54815f
Qiankun Xie <xieqiankun@wustl.edu>
2017-02-28 14:46:07 -0600

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,26 @@
 import { Component } from '@angular/core';
+import {Hero} from './hero';
+
 @Component({
-  selector: 'my-app',
-  template: `
-    <h1>{{title}}</h1>
-    <h2>My favorite hero is: {{myHero}}</h2>
-    <p>Heroes:</p>
-    <ul>
-      <li *ngFor="let hero of heroes">
-        {{ hero }}
-      </li>
-    </ul>
-  `
+  	selector: 'my-app',
+	template: `
+	  <h1>{{title}}</h1>
+	  <h2>My favorite hero is: {{myHero.name}}</h2>
+	  <p>Heroes:</p>
+	  <ul>
+	    <li *ngFor="let hero of heroes">
+	      {{ hero.name }}
+	    </li>
+	  </ul>
+	`
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
-  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
-  myHero = this.heroes[0];
+  	title = 'Tour of Heroes';
+	heroes = [
+	  new Hero(1, 'Windstorm'),
+	  new Hero(13, 'Bombasto'),
+	  new Hero(15, 'Magneta'),
+	  new Hero(20, 'Tornado')
+	];
+	myHero = this.heroes[0];
 }
